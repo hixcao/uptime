1fef86886dd1401a334382c798ec13086b03f32b
Emily  Johnson <39901135+hixcao@users.noreply.github.com>
2026-02-02 14:24:17 +0800

    Update config.js

--- config.js
+++ config.js
@@ -6,10 +6,7 @@ window.Config = {
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
-    'ur2671488-fb491a80091166a48bc66b17',
-    //'m797638073-924f5efed86446cce1c95ae1', // 周润发的博客 API key
-    //'m797713213-1c83620d56db599960ad0244', // 周润发的公益订阅 API key
-    //'m797713331-835664f191b589688f122e00', // 周润发的公益订阅器 API key
+    'ur2702871-3ea270370147faa3d8d2fc52',
   ],
 
   // 日志天数
@@ -20,18 +17,8 @@ window.Config = {
 
   // 导航栏菜单
   Navi: [
-    {
-      text: '主页',
-      url: 'https://uptime.zrfme.com/'
-    },    
-    {
-      text: 'GitHub',
-      url: 'https://github.com/shaoyouvip/uptime/'
-    },
-    {
-      text: '博客',
-      url: 'https://blog.zrf.me/'
-    },
+    
   ],
 };
+
 
